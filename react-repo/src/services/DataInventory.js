568329e7c979442d41d290c95d9dab18b1251b8e
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-08-25 19:59:53 -0400

    update

--- react-repo/src/services/DataInventory.js
+++ react-repo/src/services/DataInventory.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default async function GetInventoryData() {
-    let Cust_ID;
     let response = await axios.get(`http://127.0.0.1:8000/inventory/`);
     return response.data;
 };
